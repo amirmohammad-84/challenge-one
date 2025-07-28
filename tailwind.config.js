019eb68127f2cbacd892ff4a8010581972652617
amirmohammad <ahmadi.amirmohammad84@gmail.com>
2025-07-28 15:23:39 +0330

    final-redux

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,13 +2,13 @@
 module.exports = {
   content: [
     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}", // مسیر فایل‌های سورس پروژه رو متناسب با پروژه‌ات تغییر بده
+    "./src/**/*.{js,ts,jsx,tsx}", 
   ],
   theme: {
     extend: {
       fontFamily: {
         'vazir': ['Vazir', 'sans-serif'],
-        'yekan': ['Yekan', 'sans-serif'],  // این خط اضافه شده
+        'yekan': ['Yekan', 'sans-serif'],  
       },
     },
   },
